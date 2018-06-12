7f4e31fa6cca514a8e5ccf60b133ab1d855a0310
dosir <517441830@qq.com>
2018-06-12 20:50:44 +0800

    add font file and add btn style

--- src/components/cropper.vue
+++ src/components/cropper.vue
@@ -1,9 +1,35 @@
 <template>
 <section class="crop-wrapper">
   <header class="bar" ref="bar">
-    <button class="btn btn-preview" @click="doPreview">预览</button>
-    <button class="btn btn-yes" @click="yes">确定</button>
-    <button class="btn btn-cancel" @click="cancel">取消</button>
+
+    <div class="btn-wrap">
+    <button class="btn btn-preview" @click="doPreview">
+      <svg class="crop-icon" aria-hidden="true">
+        <use xlink:href="#icon-sousuo"></use>
+      </svg>
+      预览</button>
+    <button class="btn btn-yes" @click="yes">
+      <svg class="crop-icon" aria-hidden="true">
+        <use xlink:href="#icon-tupiancaijian"></use>
+      </svg>
+      确定</button>
+    <button class="btn btn-cancel" @click="cancel">
+      <svg class="crop-icon" aria-hidden="true">
+        <use xlink:href="#icon-cancel"></use>
+      </svg>
+      取消</button>
+      </div>
+      <div class="control" v-if="!isH5">
+        <button class="btn btn-enlarge" @click="zoomIn">
+        <svg class="crop-icon" aria-hidden="true">
+          <use xlink:href="#icon-enlarge"></use>
+        </svg>
+        放大</button><button class="btn btn-reduce" @click="zoomOut">
+        <svg class="crop-icon" aria-hidden="true">
+          <use xlink:href="#icon-reduce"></use>
+        </svg>
+        缩小</button>
+      </div>
   </header>
 
     <!-- <div class="model" :style="{top: modelTop}"></div> -->
@@ -19,6 +45,7 @@
 import axios from "axios";
 import Cropper from "cropperjs"
 import 'cropperjs/dist/cropper.css';
+import '@/assets/font/iconfont.js';
 /* TODO:
 1. 窗口调整的时候，动态计算可编辑区域的长宽高
 2. 预览功能
@@ -51,7 +78,8 @@ export default {
     modelTop: 0,
     showPreview: false,
     wrapperStyle: {'width': '100%', 'height': '100%'},
-    previewStyle: {'width': '500px', 'height': '300px'}
+    previewStyle: {'width': '500px', 'height': '300px'},
+    isH5: true
   }},
   computed: {
     config() {
@@ -66,6 +94,11 @@ export default {
   mounted() {
     let $bar = this.$refs.bar;
     this.modelTop = $bar.clientHeight;
+    let winWidth = document.body.clientWidth;
+    if(winWidth > 751) {
+      // 小于ipad都认为是mobile
+      this.isH5 = false;
+    }
     this.init();
   },
   methods: {
@@ -166,17 +199,43 @@ export default {
     },
     cancel() {
       $cropper.reset();
+    },
+    zoomIn() {
+      $cropper.zoom(0.1);
+    },
+    zoomOut() {
+      $cropper.zoom(-0.1);
     }
   }
 }
 </script>
 <style>
 .crop-wrapper {position: fixed;top: 0;left: 0;bottom: 0;right: 0;}
-.crop-wrapper .bar {padding: 6px 22px;background: #fff;}
-.crop-wrapper .bar .btn {padding: 3px 6px;margin-left: 5px;margin-right: 5px;}
-.crop-wrapper .content {}
+.crop-wrapper .bar {position: relative;padding: 8px 22px;background: #fff;font-size: 14px;}
+.crop-wrapper .bar button {outline: none;}
 .crop-wrapper .model {position: absolute;left: 0;right: 0;bottom: 0;background-color: rgba(0,0,0,0.5);}
 .crop-wrapper .core>img {max-width: 100%}
-.crop-wrapper .preview-wrap {position: fixed;top: 0;left: 0;right: 0;bottom: 0;margin: 0;background-color: rgba(0,0,0,0.7);z-index: 999;}
+.crop-wrapper .preview-wrap {position: fixed;top: 0;left: 0;right: 0;bottom: 0;margin: 0;background-color: rgba(0,0,0,0.8);z-index: 999;}
 .crop-wrapper .preview-wrap .preview {position: absolute;top: 50%;left: 50%;overflow: hidden;}
+
+.crop-wrapper .bar .btn-wrap {position: absolute; right: 22px;top: 6px;}
+.crop-wrapper .btn-wrap .btn {padding: 6px 14px;margin-left: 7px;margin-right: 7px;border-radius: 3px;color: #fff;border: 1px solid #dcdfe6;}
+.crop-wrapper .btn-wrap .btn-preview {color: #409eff;background: #ecf5ff;border-color: #b3d8ff;}
+.crop-wrapper .btn-wrap .btn-yes {color: #67c23a;background: #f0f9eb;border-color: #c2e7b0;}
+.crop-wrapper .btn-wrap .btn-cancel {color: #909399;background: #f4f4f5;border-color: #d3d4d6;}
+.crop-wrapper .btn-wrap .btn:hover {color: #fff;}
+.crop-wrapper .btn-wrap .btn-preview:hover {background: #409eff;}
+.crop-wrapper .btn-wrap .btn-yes:hover {background: #67c23a;}
+.crop-wrapper .btn-wrap .btn-cancel:hover {background: #909399;}
+
+.crop-wrapper .control .btn {padding: 6px 14px;color: #fff;background-color: #409eff;border-color: #409eff;}
+.crop-wrapper .control .btn:hover {background-color: #0062cc;border-color: #005cbf;}
+.crop-wrapper .control .btn-enlarge {border-top-left-radius: 3px;border-bottom-left-radius: 3px;border-right-color: #74b3f7;}
+.crop-wrapper .control .btn-reduce {border-top-right-radius: 3px;border-bottom-right-radius: 3px;}
+/* icon style */
+.crop-wrapper .crop-icon {width: 1em; height: 1em;vertical-align: -0.15em;fill: currentColor;overflow: hidden;}
+
+@media screen and (max-width: 752px) {
+.crop-wrapper .bar .btn-wrap {position: relative;right: auto;top: auto;font-size: 12px;}
+}
 </style>
